2d4769aeceb8211d89187962c7da0351d26c5cba
Stella Steinfeld <stella.sblanco@gmail.com>
2023-02-26 22:01:39 -0300

    solv host

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "172.31.27.95"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi-database"),
       user: env("DATABASE_USERNAME", ""),

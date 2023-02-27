1d24dcfb356b399031ddb4b9d347c6ed3aa3f738
Stella Steinfeld <stella.sblanco@gmail.com>
2023-02-26 21:28:41 -0300

    conf aws

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,14 @@
 const path = require('path');
 
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
+      database: env("DATABASE_NAME", "strapi-database"),
       user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      password: env("DATABASE_PASSWORD", "strapi-database2023"),
     },
     useNullAsDefault: true,
   },
